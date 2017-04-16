8448d74343749763771c34183a7345d98ac24752
haniokasai <htek@haniokasai.com>
2017-04-16 22:51:06 +0900

    Add files via upload

---
+++ enablepackettest/src/haniokasai/threadtest.php
@@ -0,0 +1,34 @@
+<?php
+namespace haniokasai;
+/**
+ * Created by PhpStorm.
+ * User: htek
+ * Date: 2017/04/09
+ * Time: 0:51
+ */
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\protocol\SetCommandsEnabledPacket;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+
+class threadtest extends PluginBase implements Listener{
+    public function onEnable(){
+        Server::getInstance()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    public function  onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $pk = new SetCommandsEnabledPacket();
+        $pk->enabled= false;
+        $player->dataPacket($pk);
+
+        $pk2 = new enablepk();
+        $pk2->en = true;
+        $player->dataPacket($pk);
+    }
+
+
+
+}
